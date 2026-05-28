ac89cc37678e877623cc90b567315665eb1b0b3c
Diobe Belle <philipewang120@gmail.com>
2026-05-28 19:40:08 +0100

    fix image generation on landing page 1

--- client/src/pages/LandingPage.jsx
+++ client/src/pages/LandingPage.jsx
@@ -114,20 +114,15 @@ function LandingPage() {
   useEffect(() => {
     async function fetchMovies() {
       try {
-        const res = await axios.get(
-  "https://api.themoviedb.org/3/discover/movie",
-  {
-    withCredentials: false,
-
-    params: {
-      api_key: import.meta.env.VITE_TMDB_KEY,
-      language: "en-US",
-      sort_by: "popularity.desc",
-      include_adult: false,
-      page: Math.floor(Math.random() * 10) + 1,
-    },
-  }
-);
+  const res = await axios.get( "https://api.themoviedb.org/3/discover/movie", 
+    { withCredentials: false, 
+      headers: { Authorization: `Bearer ${import.meta.env.VITE_TMDB_TOKEN}`, }, 
+      params: { language: "en-US", 
+                sort_by: "popularity.desc", 
+                include_adult: false, 
+                page: Math.floor(Math.random() * 10) + 1, },
+              
+        } );
 
         const results = res.data.results || [];
 
