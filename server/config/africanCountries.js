ed8e6d74cff55d3acffba3a0966e025e93216280
Diobe Belle <philipewang120@gmail.com>
2026-05-28 12:46:47 +0100

    Change AfricanPage logic

---
+++ server/config/africanCountries.js
@@ -0,0 +1,53 @@
+
+
+export const ALL_AFRICA =
+  "NG|ZA|EG|MA|GH|KE|TN|DZ|CM|SN|CI|TZ|UG|AO|ET|ZW|ZM|MW|MZ|BW|NA|RW|CD|CG|GA|TD|NE|ML|BF|GN|SL|LR|GM|GW|CV|MR|SD|SO|DJ|ER|LY|TG|BJ|MG|MU|SC";
+
+export const AFRICAN_COUNTRIES_ARRAY = [
+  "NG", // Nigeria
+  "ZA", // South Africa
+  "EG", // Egypt
+  "MA", // Morocco
+  "GH", // Ghana
+  "KE", // Kenya
+  "TN", // Tunisia
+  "DZ", // Algeria
+  "CM", // Cameroon
+  "SN", // Senegal
+  "CI", // Ivory Coast
+  "TZ", // Tanzania
+  "UG", // Uganda
+  "AO", // Angola
+  "ET", // Ethiopia
+  "ZW", // Zimbabwe
+  "ZM", // Zambia
+  "MW", // Malawi
+  "MZ", // Mozambique
+  "BW", // Botswana
+  "NA", // Namibia
+  "RW", // Rwanda
+  "CD", // DR Congo
+  "CG", // Congo
+  "GA", // Gabon
+  "TD", // Chad
+  "NE", // Niger
+  "ML", // Mali
+  "BF", // Burkina Faso
+  "GN", // Guinea
+  "SL", // Sierra Leone
+  "LR", // Liberia
+  "GM", // Gambia
+  "GW", // Guinea-Bissau
+  "CV", // Cape Verde
+  "MR", // Mauritania
+  "SD", // Sudan
+  "SO", // Somalia
+  "DJ", // Djibouti
+  "ER", // Eritrea
+  "LY", // Libya
+  "TG", // Togo
+  "BJ", // Benin
+  "MG", // Madagascar
+  "MU", // Mauritius
+  "SC", // Seychelles
+];
